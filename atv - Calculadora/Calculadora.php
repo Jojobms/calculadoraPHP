00a190ac3fc26810be614f7cd8714d807581e28c
Jonathan Bastos <163217939+Jojobms@users.noreply.github.com>
2024-08-28 19:53:13 -0300

    Update Calculadora.php

--- atv - Calculadora/Calculadora.php
+++ atv - Calculadora/Calculadora.php
@@ -1,15 +1,15 @@
 <?php
 
-$n1;
-$n2;
+$n1 = 0;
+$n2 = 0;
 $result = "???";
 $calcular;
 
 
-if(isset($_GET['n1'], $_GET['n2'], $_GET['Operação'])){
+if(isset($_GET['n1'], $_GET['n2'], $_GET['Operacao'])){
     $n1 = $_GET['n1'];
     $n2 = $_GET['n2'];
-    $calcular = $_GET['Operação'];
+    $calcular = $_GET['Operacao'];
 
     switch($calcular){
         case 'Soma':
@@ -22,7 +22,41 @@ if(isset($_GET['n1'], $_GET['n2'], $_GET['Operação'])){
             $result = $n1 * $n2;
             break;
         case 'Div':
-            $result = $n1 / $n2;
+            if ($n2 != 0) {
+                $result = $n1 / $n2;
+            } else {
+                $result = "Erro: Divisão por zero";
+            }
+            break;
+        case 'Raiz':
+            if ($n1 >= 0) {
+                $result = sqrt($n1);
+            } else {
+                $result = "Erro: Raiz de número negativo";
+            }
+            break;
+        case 'Potencia':
+            $result = pow($n1, $n2);
+            break;
+        case 'InversaoNumero':
+            if ($n1 != 0) {
+                $result = 1 / $n1;
+            } else {
+                $result = "Erro: Inversão de zero";
+            }
+            break;
+        case 'InversaoSinal':
+            $result = -$n1;
+            break;
+        case 'Seno':
+            $result = sin(deg2rad($n1)); 
+            break;
+        case 'Cosseno':
+            $result = cos(deg2rad($n1)); 
+            break;
+        case 'Tangente':
+            $result = tan(deg2rad($n1)); 
+            break;
     }
 }
 
@@ -36,7 +70,6 @@ if(isset($_GET['n1'], $_GET['n2'], $_GET['Operação'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculadora</title>
     <style>
-
         body {
             display: flex;
             justify-content: center;
@@ -59,7 +92,7 @@ if(isset($_GET['n1'], $_GET['n2'], $_GET['Operação'])){
         section {
             background-color: black;
             width: 256px;
-            height: 256px;
+            height: 400px;
             display: flex;
             justify-content: center;
             align-items: center;
@@ -73,13 +106,20 @@ if(isset($_GET['n1'], $_GET['n2'], $_GET['Operação'])){
         <form>
             <label for="num1">Número 1:</label>
             <input type="text" name="n1" id="num1">
-            <label for="num2">Número 2:</label>
+            <label for="num2">Número 2 (para operações binárias):</label>
             <input type="text" name="n2" id="num2">
-            <select name="Operação" id="op">
+            <select name="Operacao" id="op">
                 <option value="Soma">Soma</option>
                 <option value="Subt">Subtração</option>
                 <option value="Multi">Multiplicação</option>
                 <option value="Div">Divisão</option>
+                <option value="Raiz">Raiz Quadrada</option>
+                <option value="Potencia">Potência</option>
+                <option value="InversaoNumero">Inversão de Número</option>
+                <option value="InversaoSinal">Inversão de Sinal</option>
+                <option value="Seno">Seno</option>
+                <option value="Cosseno">Cosseno</option>
+                <option value="Tangente">Tangente</option>
             </select>
             <input type="submit" value="Calcular">
             <p>O resultado é: <?= $result ?></p>
